67047d418d8a513d39b51d5a1174fbb89ff41867
Rgrivas9 <rafaelgrcrvs@gmail.com>
2023-01-27 22:32:23 +0100

    1

---
+++ 01.basicAlg/app1.ts
@@ -0,0 +1,11 @@
+/* 1.1 Crea una variable llamada myFavoriteHero, asigna el valor Hulk a ella.
+1.2 Crea una variable llamada x, asigna el valor 50 a ella.
+1.3 Crea una variable llamada 'h' con el valor 5 y otra 'y' con el valor 10. 
+1.4 Crea una otra variable 'z' y asignale el valor de 'h' + 'y'. */
+
+const myFavoriteHero: string = "Hulk";
+const x: number = 50;
+const h: number = 5;
+const y: number = 10;
+const z: number = h+y;
+console.log(z)
