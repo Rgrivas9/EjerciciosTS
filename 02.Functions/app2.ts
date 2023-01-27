e871dcbb88fbbdf46351a78c0d914b7f71d3869f
Rgrivas9 <rafaelgrcrvs@gmail.com>
2023-01-27 23:34:57 +0100

    2

---
+++ 02.Functions/app2.ts
@@ -0,0 +1,18 @@
+const avengers: string[] = [
+  "Hulk",
+  "Thor",
+  "IronMan",
+  "Captain A.",
+  "Spiderman",
+  "Captain M.",
+];
+const findLongest = (list: string[]): string => {
+  let longest: string = list[0];
+  for (const word of list) {
+    if (word.length > longest.length) {
+      longest = word;
+    }
+  }
+  return longest;
+};
+console.log(findLongest(avengers))
