84d804a0632ebc20920eb215f3fedaf8bbefe6a5
Rgrivas9 <rafaelgrcrvs@gmail.com>
2023-01-28 08:48:16 +0100

    3

---
+++ 03.loops/app3.ts
@@ -0,0 +1,11 @@
+const placesToTravel: string[] = [
+  "Japon",
+  "Venecia",
+  "Murcia",
+  "Santander",
+  "Filipinas",
+  "Madagascar",
+];
+for (const place of placesToTravel) {
+  console.log(place);
+}
