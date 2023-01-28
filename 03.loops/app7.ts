847aee88bf048b6e89f7b6e9fcff3ce178bd0909
Rgrivas9 <rafaelgrcrvs@gmail.com>
2023-01-28 09:44:16 +0100

    7

--- 03.loops/app7.ts
+++ 03.loops/app7.ts
@@ -1,10 +1,10 @@
-interface Toy {
+interface Toy1 {
   id: number;
   name: string;
   sellCount: number;
 }
-const popularToys: Toy[] = [];
-const toys1: Toy[] = [
+const popularToys: Toy1[] = [];
+const toys1: Toy1[] = [
   { id: 5, name: "Buzz MyYear", sellCount: 10 },
   { id: 11, name: "Action Woman", sellCount: 24 },
   { id: 23, name: "Barbie Man", sellCount: 15 },
